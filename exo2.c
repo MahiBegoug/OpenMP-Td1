ef10058c4cb280c4904bde6f7aab567487c1f36e
Mahi Begoug <51762779+MahiBegoug@users.noreply.github.com>
2020-04-06 11:46:25 +0100

    Update exo2.c

--- exo2.c
+++ exo2.c
@@ -1,32 +1,73 @@
-#include <stdio.h>
 #include <omp.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
+#define CHUNKSIZE 100
+#define N 1000
+
+void main() {
+
+	int i , chunk ;
+	double time_spent;
+	clock_t begin,end;
+	float tab1[N] , tab2[N] ,somme[N]; 
+	chunk = CHUNKSIZE;
+
+	for(i=0;i<N;i++) tab1[i] = tab2[i] = i* 1.0;
+
+	begin = clock();
+
+/// static avec chunk
+	#pragma omp parallel shared(tab1,tab2,somme,chunk) private(i)
+	{
+		#pragma omp for schedule(static,chunk)
+			for (i=0;i<N;i++)
+				somme[i] = tab1[i] + tab2[i];
+	}
+
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Static avec chunk : %f\n",time_spent);
 
 
+/// static sans chunk
+	begin = clock();
 
-int main (int args, char *argv[])
-{
-    float a[1000], b[1000], c[1000];
-    int i, chunk;
+	#pragma omp parallel shared(tab1,tab2,somme) private(i)
+	{
+		#pragma omp for schedule(static)
+			for (i=0;i<N;i++)
+				somme[i] = tab1[i] + tab2[i];
+	}
 
-    for (i =0 ; i<1000; i++) a[i] = b [i] = i * 1.0;
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Static sans chunk : %f\n",time_spent);
+/// dynamic avec chunk
+begin = clock();
 
-    chunk = 100 ;
+	#pragma omp parallel shared(tab1,tab2,somme,chunk) private(i)
+	{
+		#pragma omp for schedule(dynamic,chunk)
+			for (i=0;i<N;i++)
+				somme[i] = tab1[i] + tab2[i];
+	}
 
-    double time_spent = 0.0;
-    clock_t begin = clock();
-    #pragma omp parallel shared(a,b,c,chunk) private(i)
-    {
-         #pragma omp for schedule(dynamic, chunk) nowait 
-                for (i=0; i<1000; i++)  
-                    c[i] = a[i] + b [i];
-    }
-   	clock_t end = clock();
-    
-    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Dynamic with chunk : %f\n",time_spent);
 
-	printf("Time elpased is %f seconds \n", time_spent);
-    
-    return (0);
+//// dynamic sans chunk
+begin = clock();
+
+	#pragma omp parallel shared(tab1,tab2,somme) private(i)
+	{
+		#pragma omp for schedule(dynamic)
+			for (i=0;i<N;i++)
+				somme[i] = tab1[i] + tab2[i];
+	}
+
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Dynamic sans chunk : %f\n",time_spent);
 }
